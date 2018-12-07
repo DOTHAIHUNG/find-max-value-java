d974b9964b2e02f1403be14c93e6225219cff00c
HUNGDT <hk2t.thaihung@gmail.com>
2018-12-07 11:37:04 +0700

    change value

--- src/findMaxValue.java
+++ src/findMaxValue.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class findMaxValue {
     public static void main(String[] args) {
-        int[] money = new int[5];
+        int[] money = new int[20];
 
         Scanner sc = new Scanner(System.in);
 
